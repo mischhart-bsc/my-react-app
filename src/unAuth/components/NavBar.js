1e3fd8edf81da6780fdf41f43977040e073c6bca
Mischa Hartmann <misch.hart@gmail.com>
2025-11-25 08:13:19 +0100

    Layout hinzugefügt

--- src/unAuth/components/NavBar.js
+++ src/unAuth/components/NavBar.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './NavBar.css';
+import React from "react";
+import "./NavBar.css";
+import tubev1 from "../../assets/tubev1.png";
 
 const NavBar = () => {
-    return (
-        <nav className="navbar-container">
-            <div className="component-label">NavBar</div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img src={tubev1} alt="Logo" className="navbar-logo" />
+      </div>
+
+      <ul className="navbar-links">
+        <li>
+          <a href="/pricing">Pricing</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+      </ul>
+
+      <button className="navbar-btn">"Do More With Video"</button>
+    </nav>
+  );
 };
 
 export default NavBar;
-
