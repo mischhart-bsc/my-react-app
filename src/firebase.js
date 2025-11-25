507498b69bfb9a4cff4120af28304040d6c821fa
Mischa Hartmann <misch.hart@gmail.com>
2025-11-25 04:09:15 +0100

    Firebase Git React

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCitiKAqoJ952SOOpjdwkvwD65YdxGstRs",
+  authDomain: "my-react-app-b708f.firebaseapp.com",
+  projectId: "my-react-app-b708f",
+  storageBucket: "my-react-app-b708f.firebasestorage.app",
+  messagingSenderId: "688169818212",
+  appId: "1:688169818212:web:85abc2fbfe31ba97a92771"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
